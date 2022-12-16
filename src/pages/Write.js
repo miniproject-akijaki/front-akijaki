e5c6dabca3d9cb778460de2695a606fd990f2082
leegaeddong <118152590+leegaeddong@users.noreply.github.com>
2022-12-16 15:41:10 +0900

    write 페이지 수정

--- src/pages/Write.js
+++ src/pages/Write.js
@@ -1,5 +1,17 @@
 const Write = () => {
-  return <div>코디 작성 페이지입니다.</div>;
+  return (
+    <section className="DtSection">
+      <p>코디작성 페이지</p>
+      <div className="DetailBackGround">
+        <div className="DetailContent">
+          <div className="Img"> 이미지가 들어갑니다</div>
+          <div className="Title"> 제목이 들어갑니다</div>
+          <div className="Content"> 내용이 들어갑니다</div>
+          <div className="price">가격이 들어갑니다</div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Write;
