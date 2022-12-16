32b864e5bfb35574544747ce4e30280c3371d51f
junyoungNA <nanasooo@naver.com>
2022-12-16 15:36:05 +0900

    Router페이지 수정

--- src/route/Router.js
+++ src/route/Router.js
@@ -3,12 +3,14 @@ import SignUp from "../pages/SignUp";
 import Write from "../pages/Write";
 import Main from "../pages/Main";
 import Login from "../pages/Login";
+import Detail from "../pages/Detail";
 
 const Router = () => {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/main" element={<Main />} />
+      <Route path="/detail" element={<Detail />} />
       <Route path="/signup" element={<SignUp />} />
       <Route path="/write" element={<Write />} />
       <Route path="*" element={<div>이거 404 페이지요</div>} />
